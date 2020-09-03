045cf8e57647a05a0cb8795c3ee6159793d553a3
Ajuogaaz <brianlinus1753@gmail.com>
2020-09-03 15:13:35 -0400

    switched to production variables

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const url = "mongodb://localhost:27017/userDB"
-//const url = "mongodb+srv://okoth:nairobi@cluster0.aleio.gcp.mongodb.net/userDB";
+//const url = "mongodb://localhost:27017/userDB"
+const url = "mongodb+srv://okoth:nairsobi@cluster0.aleio.gcp.mongodb.net/userDB";
 
 mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
@@ -70,8 +70,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL : "http://localhost:3000/auth/google/secrets",
-        //callbackURL: "https://stormy-bastion-13858.herokuapp.com/auth/google/secrets",
+        //callbackURL : "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://stormy-bastion-13858.herokuapp.com/auth/google/secrets",
         userProfileURL : "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, cb) {
